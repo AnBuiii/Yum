5c2bfe5e994da5efa88abf3dda3de23e5b50f825
AnBuiii <builehoaian2002@gmail.com>
2023-04-03 00:09:18 +0700

    update nutrition tab

--- app/src/main/java/com/anbui/yum/presentation/recipe/tabs/NutritionTab.kt
+++ app/src/main/java/com/anbui/yum/presentation/recipe/tabs/NutritionTab.kt
@@ -3,76 +3,30 @@ package com.anbui.yum.presentation.recipe.tabs
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.anbui.yum.data.model.IngredientDetail
 import com.anbui.yum.data.model.Recipe
+import com.anbui.yum.data.model.nutritionFacts
+import com.anbui.yum.presentation.recipe.component.NutritionListItem
 import com.anbui.yum.presentation.recipe.component.PieChart
 import com.anbui.yum.presentation.recipe.component.TabTopBar
+import com.anbui.yum.ui.theme.YumBlack
 import com.anbui.yum.ui.theme.YumGreen
+import com.anbui.yum.ui.theme.semi
 
 @Composable
 fun NutritionTab(
-    recipe: Recipe
+    recipe: Recipe,
 ) {
-    val pagePadding = 24.dp
-    val ingredients = listOf(
-        IngredientDetail(
-            ingredientId = "garlic cloves",
-            unit = "can",
-            amount = 4,
-            note = "asdasd",
-        ),
-        IngredientDetail(
-            ingredientId = "garlic cloves",
-            unit = "helo",
-            amount = 4,
-            note = "ok bro",
-        ),
-        IngredientDetail(
-            ingredientId = "garlic cloves",
-            unit = "",
-            amount = 4,
-            note = "hm",
-        ),
-        IngredientDetail(
-            ingredientId = "garlic cloves",
-            unit = "",
-            amount = 4,
-        ),
-        IngredientDetail(
-            ingredientId = "garlic cloves",
-            unit = "",
-            amount = 4,
-        ),
-        IngredientDetail(
-            ingredientId = "garlic cloves",
-            unit = "",
-            amount = 4,
-        ),
-        IngredientDetail(
-            ingredientId = "garlic cloves",
-            unit = "",
-            amount = 4,
-        ),
-        IngredientDetail(
-            ingredientId = "garlic cloves",
-            unit = "",
-            amount = 4,
-        ),
-        IngredientDetail(
-            ingredientId = "garlic cloves",
-            unit = "",
-            amount = 4,
-        ),
-    )
-    LazyColumn{
+    LazyColumn {
 
         item {
             TabTopBar(
@@ -90,17 +44,26 @@ fun NutritionTab(
             )
 
         }
-        item{
+        item {
             Spacer(modifier = Modifier.padding(top = 12.dp))
         }
-        item{
+        item {
             PieChart(
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 24.dp)
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 24.dp),
             )
+
         }
 
+        item {
+            NutritionListItem(style = semi)
+            Divider(modifier = Modifier.padding(horizontal = 24.dp), color = YumBlack)
+        }
 
+        items(nutritionFacts) {
+            NutritionListItem(title = it.first, amount = it.second, rda = it.third)
+            Divider(modifier = Modifier.padding(horizontal = 24.dp))
 
+        }
 
     }
 
