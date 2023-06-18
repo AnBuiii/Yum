33807a987f59003605355d8ad9d7bd8f4f6330df
AnBuiii <builehoaian2002@gmail.com>
2023-06-18 13:49:47 +0700

    fix paging

--- Yum-Client/app/src/main/java/com/anbui/yum/data/remote/RecipeRemoteMediator.kt
+++ Yum-Client/app/src/main/java/com/anbui/yum/data/remote/RecipeRemoteMediator.kt
@@ -18,99 +18,78 @@ class RecipeRemoteMediator(
     private val yumDb: YumDatabase,
     private val recipeService: RecipeService,
 ) : RemoteMediator<Int, RecipeEntity>() {
+    override suspend fun initialize(): InitializeAction {
+        return InitializeAction.LAUNCH_INITIAL_REFRESH
+    }
 
     override suspend fun load(
         loadType: LoadType,
         state: PagingState<Int, RecipeEntity>,
     ): MediatorResult {
-        return try {
-            val loadKey = when (loadType) {
-                LoadType.REFRESH -> {
-                    val remoteKeys = getRemoteKeyClosestToCurrentPosition(state)
-
-                    remoteKeys?.nextPage?.minus(1) ?: 1
-                }
-
-                LoadType.PREPEND -> {
-                    Log.d("a", "asd")
-                    val remoteKeys = getRemoteKeyForFirstItem(state)
-                    val nextPage = remoteKeys?.nextPage
-                        ?: return MediatorResult.Success(
-                            endOfPaginationReached = remoteKeys != null,
-                        )
-                    nextPage
-
-//                    return MediatorResult.Success(
-//                        endOfPaginationReached = true,
-//                    )
-                }
-
-                LoadType.APPEND -> {
-                    Log.d("Mediator", "append")
-
-                    val remoteKeys = getRemoteKeyForLastItem(state)
-                    val nextPage = remoteKeys?.nextPage
-                        ?: return MediatorResult.Success(
-                            endOfPaginationReached = remoteKeys != null,
-                        )
-                    nextPage
-
-//                    val lastItem = state.lastItemOrNull()
-//                    if(lastItem == null) {
-//                        return MediatorResult.Success(endOfPaginationReached = true)
-//                    } else {
-//                        (yumDb.recipeDao.size() / state.config.pageSize) + 1
-//                    }
-                }
+        val loadKey = when (loadType) {
+            LoadType.REFRESH -> {
+                val remoteKeys = getRemoteKeyClosestToCurrentPosition(state)
+                remoteKeys?.nextPage?.minus(1) ?: 1 // start page index
             }
 
+            LoadType.PREPEND -> {
+                Log.d(
+                    "Mediator",
+                    "prepend",
+                )
+
+                val remoteKeys = getRemoteKeyForFirstItem(state)
+                val prevPage = remoteKeys?.prevPage
+                    ?: return MediatorResult.Success(endOfPaginationReached = remoteKeys != null)
+                prevPage
+            }
+
+            LoadType.APPEND -> {
+                Log.d(
+                    "Mediator",
+                    "append",
+                )
+                val remoteKeys = getRemoteKeyForLastItem(state)
+                val nextPage = remoteKeys?.nextPage
+                    ?: return MediatorResult.Success(
+                        endOfPaginationReached = remoteKeys != null,
+                    )
+                nextPage
+            }
+        }
+        try {
             val recipes = recipeService.getRecipes(
                 page = loadKey,
                 pageCount = state.config.pageSize,
             )
-
             val endOfPaginationReached = recipes.isEmpty()
 
-            val prevPage = if (loadKey == 1) null else loadKey - 1
-            val nextPage = if (endOfPaginationReached) null else loadKey + 1
-
             yumDb.withTransaction {
-//                if(loadType == LoadType.REFRESH) {
-//                    yumDb.recipeDao.clearAll()
-//                }
-//                val recipeEntities = recipes.map { it.toRecipeEntity() }
-//                yumDb.recipeDao.upsertAll(recipeEntities)
-
                 if (loadType == LoadType.REFRESH) {
                     yumDb.recipeDao.clearAll()
                     yumDb.recipeRemoteDao.deleteAllRemoteKeys()
                 }
-                val keys = recipes.map { unsplashImage ->
+                val prevPage = if (loadKey == 1) null else loadKey - 1
+                val nextPage = if (endOfPaginationReached) null else loadKey + 1
+
+                val keys = recipes.map { recipeEntity ->
                     RecipeRemoteKey(
-                        id = unsplashImage.id,
+                        id = recipeEntity.id,
                         prevPage = prevPage,
                         nextPage = nextPage,
                     )
                 }
-
                 val recipeEntities = recipes.map { it.toRecipeEntity() }
                 yumDb.recipeDao.upsertAll(recipeEntities)
                 yumDb.recipeRemoteDao.addAllRemoteKeys(keys)
             }
-
-
-
-
-
-
-
-            MediatorResult.Success(
-                endOfPaginationReached = recipes.isEmpty()
+            return MediatorResult.Success(
+                endOfPaginationReached = recipes.isEmpty(),
             )
         } catch (e: IOException) {
-            MediatorResult.Error(e)
+            return MediatorResult.Error(e)
         } catch (e: Exception) {
-            MediatorResult.Error(e)
+            return MediatorResult.Error(e)
         }
     }
 
@@ -128,8 +107,8 @@ class RecipeRemoteMediator(
         state: PagingState<Int, RecipeEntity>,
     ): RecipeRemoteKey? {
         return state.pages.firstOrNull { it.data.isNotEmpty() }?.data?.firstOrNull()
-            ?.let { unsplashImage ->
-                yumDb.recipeRemoteDao.getRemoteKeys(id = unsplashImage.id)
+            ?.let { recipeEntity ->
+                yumDb.recipeRemoteDao.getRemoteKeys(id = recipeEntity.id)
             }
     }
 
@@ -137,8 +116,8 @@ class RecipeRemoteMediator(
         state: PagingState<Int, RecipeEntity>,
     ): RecipeRemoteKey? {
         return state.pages.lastOrNull { it.data.isNotEmpty() }?.data?.lastOrNull()
-            ?.let { unsplashImage ->
-                yumDb.recipeRemoteDao.getRemoteKeys(id = unsplashImage.id)
+            ?.let { recipeEntity ->
+                yumDb.recipeRemoteDao.getRemoteKeys(id = recipeEntity.id)
             }
     }
 
